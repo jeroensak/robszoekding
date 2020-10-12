9b7bfce62a02ea7b18f4c117fb623c97781b329c
Jeroen Sak <jsak@garagesoftware.nl>
2020-10-12 11:35:49 +0200

    wip glhf

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+import React from "react";
+import { useDebounce } from "use-debounce/lib";
+
+const baseUrl = "http://localhost:5000";
+
+export const useSearch = (searchString: string) => {
+  const [debouncedValue] = useDebounce(searchString, 200);
+  const [result, setResult] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    if (debouncedValue.length > 3) {
+      performSearch(debouncedValue).then(setResult);
+    }
+  }, [debouncedValue]);
+
+  return result;
+};
+
+const performSearch = (value: string) => {
+  return fetch(`${baseUrl}/search/completedorder`, {
+    method: "POST",
+    // mode: 'cors',
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(getSearchBody(value)),
+  })
+    .then((res) => res.json())
+    .catch((e) => console.error("e", e));
+};
+
+const getSearchBody = (value: string) => ({
+  searchTerm: value,
+  minimumShouldMatch: 10,
+  searchOperator: 0,
+  skip: 0,
+  take: 100,
+  maxFilterResults: 100,
+  searchMode: 0,
+});
