162a164045e16fdde57e15344ccb758fb73a2976
Jeroen Sak <jsak@garagesoftware.nl>
2020-10-13 08:49:00 +0200

    1 filter per row

--- src/FilterBucket.tsx
+++ src/FilterBucket.tsx
@@ -8,13 +8,13 @@ import {
 import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 import React from "react";
 import { IFilter } from "./Filters";
-import Chip from '@material-ui/core/Chip';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import Chip from "@material-ui/core/Chip";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
@@ -22,9 +22,12 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 12,
     marginLeft: -10,
     height: 20,
-      },
+  },
   shapeCircle: {
-    borderRadius: '50%',
+    borderRadius: "50%",
+  },
+  formControlLabel: {
+    width: "100%",
   },
 }));
 
@@ -61,27 +64,31 @@ const Filter = ({
     }
   };
 
-  function unCamelCase (str){
-    return str
+  function unCamelCase(str) {
+    return (
+      str
         // insert a space between lower & upper
-        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .replace(/([a-z])([A-Z])/g, "$1 $2")
         // space before last upper in a sequence followed by lower
-        .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+        .replace(/\b([A-Z]+)([A-Z])([a-z])/, "$1 $2$3")
         // uppercase the first character
-        .replace(/^./, function(str){ return str.toUpperCase(); })
-        
-}
+        .replace(/^./, function (str) {
+          return str.toUpperCase();
+        })
+    );
+  }
 
-  var name =unCamelCase(  filter.name).toUpperCase();
+  var name = unCamelCase(filter.name).toUpperCase();
   const classes = useStyles();
   return (
-      <Paper key={filter.name} className={className}>
-        <Typography>{name}</Typography>
-        <Divider />
-        <FormGroup row>
-          {filter.buckets.map((bucket: any, index: number) => (
-            <>
+    <Paper key={filter.name} className={className}>
+      <Typography>{name}</Typography>
+      <Divider />
+      <FormGroup row>
+        {filter.buckets.map((bucket: any, index: number) => (
+          <>
             <FormControlLabel
+              className={classes.formControlLabel}
               key={index}
               control={
                 <Checkbox
@@ -95,12 +102,11 @@ const Filter = ({
               }
               label={`${bucket.value} (${bucket.hits})`}
             />
-             {/* <Chip label={bucket.hits} size="small" className={classes.shape} color="primary"/> */}
-            </>
-          ))}
-        </FormGroup>
-      </Paper>
-    
+            {/* <Chip label={bucket.hits} size="small" className={classes.shape} color="primary"/> */}
+          </>
+        ))}
+      </FormGroup>
+    </Paper>
   );
 };
 
